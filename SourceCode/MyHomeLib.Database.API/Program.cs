4c13687ea0b39081e07cb338bbc16f2415e76b18
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2020-01-12 16:35:41 +0200

    Fix warnings

--- SourceCode/MyHomeLib.Database.API/Program.cs
+++ SourceCode/MyHomeLib.Database.API/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace MyHomeLib.Database.API
 {
-  public class Program
+  public static class Program
   {
     public static void Main(string[] args)
     {
