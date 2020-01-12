e297ac8367e9f1bc6b08766f45560e0813147bd7
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2020-01-12 16:23:38 +0200

    Add MyHomeLib.Database.API project

---
+++ SourceCode/MyHomeLib.Database.API/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyHomeLib.Database.API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BookController : ControllerBase
+  {
+    private readonly IDatabaseRepository repository;
+
+    public BookController(IDatabaseRepository repository)
+    {
+      this.repository = repository;
+    }
+
+    // GET: api/Book/test
+    [HttpGet("{titlePart}", Name = "Get")]
+    public IEnumerable<BookInfo> Get(string titlePart)
+    {
+      if (string.IsNullOrWhiteSpace(titlePart) || titlePart.Length<5)
+        return null;
+      return repository.GetBooksByTitle(titlePart);
+    }
+  }
+}
