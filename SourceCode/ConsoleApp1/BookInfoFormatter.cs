df2e71f3716c3c254c9265b19c75d81a32600b46
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2020-02-05 18:11:03 +0200

    Console application was improved: 1. Configuration settings 2. Print formatter 3. Extended logic

---
+++ SourceCode/ConsoleApp1/BookInfoFormatter.cs
@@ -0,0 +1,24 @@
+﻿using MyHomeLib.Database;
+using System;
+
+namespace ConsoleApp1
+{
+  public static class BookInfoFormatter
+  {
+    public static string ToFormattedString(this BookInfo b)
+    {
+      if (b == null) 
+        throw new ArgumentNullException(nameof(b));
+      return $"{b.AuthorLastName} {b.AuthorFirstName}: \n    {GetSeriesTitle(b.SeriesTitle)}{GetSeqNumber(b.SeqNumber)}{b.BookTitle}. {b.UpdateDate}. \\{b.Folder}\\{b.FileName + b.Ext}";
+    }
+    private static string GetSeqNumber(int value)
+    {
+      return value == 0 ? "" : value + ". ";
+    }
+
+    private static string GetSeriesTitle(string value)
+    {
+      return string.IsNullOrWhiteSpace(value) ? "" : value + " - ";
+    }
+  }
+}
--- SourceCode/ConsoleApp1/Program.cs
+++ SourceCode/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using LibRusEc.FileStorage;
 using System;
 using System.IO;
 using System.Linq;
+using System.Configuration;
 
 namespace ConsoleApp1
 {
@@ -10,36 +11,85 @@ namespace ConsoleApp1
   {
     static void Main(string[] args)
     {
-      var bookName = args.Any() ? args[0] : "БЛЭКАУТ";
-      MyCall2(bookName);
+      var bookName = args.Any() ? args[0] : "%оборотень%";
+      MyCall(bookName);
     }
-    private static void MyCall2(string bookName)
+    private static void MyCall(string bookName)
     {
       //var dbPath = @"D:\New\librusec_local_fb2.hlc2";
       //var storagePath = @"D:\_Lib.rus.ec - Официальная\Lib.rus.ec";
-      var dbPath = @"D:\Downloads\librusec_local_fb2.hlc2";
-      var storagePath = @"E:\Disk_D\_Lib.rus.ec - Официальная\Lib.rus.ec";
+      //var dbPath = @"D:\Downloads\librusec_local_fb2.hlc2";
+      //var storagePath = @"E:\Disk_D\_Lib.rus.ec - Официальная\Lib.rus.ec";
+      var dbPath = ConfigurationManager.AppSettings["dbPath"];
+      var storagePath = ConfigurationManager.AppSettings["storagePath"]; ;
 
+      var books = FindBooks(bookName, dbPath);
+      PrintBooks(books);
+      SaveBooks(storagePath, books);
+    }
 
+    private static BookInfo[] FindBooks(string bookName, string dbPath)
+    {
       var repo = new DatabaseRepository(dbPath);
-      var books = repo.GetBooksByTitle(bookName);
-      Console.WriteLine($"Books found: {books.Count()}");
+      var books = repo.GetBooksByTitle(bookName).OrderBy(b => b).ToArray();
+      Console.WriteLine($"Books found: {books.Length}");
+      return books;
+    }
 
-      foreach (var b in books)
-        Console.WriteLine($"{b.AuthorFirstName} {b.AuthorLastName}: {b.SeriesTitle} - {b.BookTitle}");
-
-      var book = books.First();
-      string fileName = book.FileName + book.Ext;
-
-      //var storageRepo = new FileStorageRepository();
-      var storageRepo = new FileStorageRepository(storagePath);
-      using var stream = storageRepo.GetFile(book.Folder, fileName);
-      if(stream != null)
+    private static void SaveBooks(string storagePath, BookInfo[] books)
+    {
+      while (true)
       {
-        using var fileStream = File.Create(Path.Combine(@"D:\", $"{book.Folder}_{fileName}"));
-        stream.CopyTo(fileStream);
+        var bookIndex = GetBookIndex(books.Length + 1);
+        if (bookIndex == 0)
+          break;
+        SaveBook(storagePath, books[bookIndex - 1]);
       }
     }
 
+    private static void PrintBooks(BookInfo[] books)
+    {
+      int index = 1;
+      foreach (var b in books)
+        Console.WriteLine($"{index++}. " + b.ToFormattedString());
+    }
+
+    private static void SaveBook(string storagePath, BookInfo book)
+    {
+      string fileName = book.FileName + book.Ext;
+
+      var storageRepo = new FileStorageRepository(storagePath);
+      using var stream = storageRepo.GetFile(book.Folder, fileName);
+      if (stream != null)
+      {
+        string path = Path.Combine(@"D:\", $"{book.Folder}_{fileName}");
+        using var fileStream = File.Create(path);
+        stream.CopyTo(fileStream);
+        Console.WriteLine($"The book has been saved to {path}.");
+      }
+    }
+
+    private static int GetBookIndex(int maxValidValue)
+    {
+      while (true)
+      {
+        Console.WriteLine("Enter book index for download or 'q' for exit");
+        var value = Console.ReadLine();
+
+        if (value == "q") return 0; 
+
+        if (!int.TryParse(value, out int bookIndex))
+        {
+          Console.WriteLine("Invalid value");
+          continue;
+        }
+        if (bookIndex > maxValidValue || bookIndex <= 0)
+        {
+          Console.WriteLine("Invalid book index");
+          continue;
+        }
+        return bookIndex;
+      }
+    }
   }
 }
